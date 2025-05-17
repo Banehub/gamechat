0bd09ce0661fa9fc6ebd64a2931da45073247f88
Willie <83033083+Banehub@users.noreply.github.com>
2025-05-17 14:09:22 +0200

    FML

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,16 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
